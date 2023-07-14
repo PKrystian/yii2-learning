9c4679f9229223d1a7ed51ace109929a6074e4d9
PKrystian <krystian.pinczak2001@gmail.com>
2023-07-14 22:57:31 +0200

    youtube-clone, 4:10:00

---
+++ YoutubeClone/frontend/views/video/_video_item.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @var $model \common\models\Video */
+
+use yii\helpers\Url;
+
+?>
+
+<div class="card m-3" style="width: 18rem;">
+    <a href="<?= Url::to(['video/view', 'id' => $model->video_id]) ?>">
+        <div class="embed-responsive embed-responsive-16by9">
+            <video class="embed-responsive-item"
+                poster="<?= $model->getThumbnailLink() ?>"
+                src="<?= $model->getVideoLink() ?>"
+                width=100%
+                height=100%>
+            </video>
+        </div>
+    </a>
+    <div class="card-body p-2">
+        <h6 class="card-title m-0"><?= $model->title ?></h6>
+        <p class="text-muted card-text m-0">
+                <?= $model->createdBy->username ?>
+        </p>
+        <p class="text-muted card-text m-0">
+                <?= '140 views ' . \Yii::$app->formatter->asRelativeTime($model->created_at) ?>
+        </p>
+  </div>
+</div>
